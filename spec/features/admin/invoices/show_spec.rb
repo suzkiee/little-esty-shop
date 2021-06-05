9cd32ad276dc8be22ddc136066f5e293613f557b
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-04 20:08:22 -0600

    Creates inital invoice show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices show page', type: :feature do
-  before(:each) do
+  before(:all) do
+    @invoice = Invoice.first
   end
 
   describe 'page appearance' do
+    it 'has all info about that invoice' do
+      visit "/admin/invoices/#{@invoice.id}"
+      date = @invoice.created_at.strftime("%A, %B %d, %Y")
+      expect(page).to have_content @invoice.status.capitalize
+      expect(page).to have_content @invoice.id
+      expect(page).to have_content date
+      expect(page).to have_content @invoice.customer.first_name
+      expect(page).to have_content @invoice.customer.last_name
+    end
   end
 
   describe 'page functionality' do
