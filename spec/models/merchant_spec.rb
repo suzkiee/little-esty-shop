7040f01594f930732c22789c3820636d1dd471d3
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-06 16:43:06 +0200

    Updates test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Merchant do
   before(:each) do
-    @gary = Merchant.first
-    @item_1 = @gary.items.create!(name: "Shirt", description: "Gary wears it", unit_price: 55, enabled: "enabled")
+    @gary = Merchant.find(1)
+    @item_1 = Item.find(1)
     @item_2 = @gary.items.create!(name: "Pants", description: "Gary does not wear it", unit_price: 6, enabled: "disabled")
   end
 
@@ -15,14 +15,12 @@ RSpec.describe Merchant do
   end
 
   describe 'instance methods' do
-    it '#enabled_items' do
-      it 'collects enabled items' do
-        expect(@gary.enabled_items).to eq([@item_1])
-      end
+    it 'collects enabled items' do
+      expect(@gary.enabled_items).to include(@item_1)
+    end
 
-      it 'collects disabled items' do
-       expect(@gary.disabled_items).to eq([@item_2])
-     end
+    it 'collects disabled items' do
+      expect(@gary.disabled_items).to eq([@item_2])
     end
   end
 end
