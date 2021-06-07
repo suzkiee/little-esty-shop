950d6aa1aa2449a1ebaea4f60b406004d125d985
Suzie K <suziekim.dev@gmail.com>
2021-06-07 15:22:28 -0400

    Refactor ::not_yet_shipped test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item do
   before(:each) do
-    @item_day = Item.find(1)
-    @merchant = Merchant.create!(name: 'Sally Handmade')
-    @item =  @merchant.items.create!(name: 'Qui Esse', description: 'Lorem ipsim', unit_price: 75107)
-    @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 20112)
-    @item_3 = @merchant.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 55542)
-    @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
-    @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 1)
-    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 1)
-    InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 2)
-end
+  end
 
   describe 'relationships' do
     it {should have_many :invoice_items}
@@ -21,15 +11,11 @@ end
 
   describe 'class methods' do
     it '::not_yet_shipped' do
+      shippable_items = Item.not_yet_shipped
 
-      expect(Item.not_yet_shipped).to eq([@item, @item_2])
-    end
-  end
-
-  describe 'instance methods' do
-    it '#top_day' do
-
-      expect(@item_day.top_day.to_s).to eq("2012-03-25")
+      expect(shippable_items.size).to eq(135)
+      expect(shippable_items.first.name).to eq("Item Hic Soluta")
+      expect(shippable_items.last.name).to eq("Item Ratione Dolor")
     end
   end
 end
