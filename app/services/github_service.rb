59a0f6b6cefd85b3c87e6ff7b63dbdf160e7433a
Suzie K <suziekim.dev@gmail.com>
2021-06-08 16:10:41 -0400

    Add files

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,6 +2,7 @@ class GithubService
   REPO_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop'
   USER_PATH = 'https://api.github.com/users/'
   PULLS_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/pulls?state=all'
+  COLLABORATOR_PATH = 'https://api.github.com/projects/42/collaborators'
 
   # This method can intake any path so API calls can be flexible.
   def self.connect(path)
@@ -21,14 +22,17 @@ class GithubService
     connect(PULLS_PATH)
   end
 
+  def self.all_collaborators
+    connect(COLLABORATOR_PATH)
+  end
+ 
   # Could add better error if API response is not 200, but fine for our purposes
   def self.validate_connection(response)
     if !response.status == 200
-      # Incorporate flash alert here?
+      # Incorporate flash alert here? yes
       nil
     else
       JSON.parse(response.body, symbolize_names: true)
     end
   end
-
 end
