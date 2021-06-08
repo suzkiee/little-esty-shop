b29128cc909fab22e9b87d757d068d3293ee71c8
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-08 16:19:15 -0600

    Testing API for github

--- app/poros/github_repo.rb
+++ app/poros/github_repo.rb
@@ -1,9 +1,19 @@
 class GithubRepo
-  attr_reader :collaborators
-  
+  attr_reader :commits_hash
+
   def initialize
     @repo_prs = GithubService.num_pull_requests
-    @collaborators = GithubService.all_collaborators
+    @commits = GithubService.all_collaborators
+    @thing = GithubService.connect_user
+
+    project_team = ['netia1128', 'suzkiee', 'jamogriff', 'Jaybraum']
+    @commits_hash = @commits.each_with_object(Hash.new(0)) do |commit, hash|
+      require 'pry'; binding.pry
+      if project_team.include? commit[:login]
+        hash[commit[:login]] += 1
+      end
+    end
+
     require 'pry'; binding.pry
     #initialize repo object with base endpoint with access to different attributes
     #set constant with the base endpoint and initialize with url paths
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,17 +2,17 @@ require 'token'
 
 class GithubService
   REPO_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop'
-  USER_PATH = 'https://api.github.com/users/'
+  PRSPECIFIC_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/pulls/74/commits'
   PULLS_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/pulls?state=all'
-  COLLABORATOR_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/collaborators'
+  COLLABORATOR_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/commits'
 
   # :commits_url=>"https://api.github.com/repos/suzkiee/little-esty-shop/commits{/sha}",
   # :git_commits_url=>"https://api.github.com/repos/suzkiee/little-esty-shop/git/commits{/sha}",
 
   # This method can intake any path so API calls can be flexible.
   def self.connect(path)
-    faraday = Faraday.new(headers: {'Authorization' => "token " + TOKEN})
-    validate_connection(faraday.get(path))
+    # faraday = Faraday.new(headers: {'Authorization' => "token " + TOKEN})
+    validate_connection(Faraday.get(path))
   end
 
   # The following two methods utilize pre-specified paths that are common
@@ -20,8 +20,8 @@ class GithubService
     connect(REPO_PATH)
   end
 
-  def self.connect_user(username)
-    connect(USER_PATH + username)
+  def self.connect_user@
+    connect(PRSPECIFIC_PATH)
   end
 
   def self.num_pull_requests
