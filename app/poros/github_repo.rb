b041e66a895abb92ab49778cc63570e4b092a976
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-08 21:53:24 -0600

    Refactor GithubRepo; refactor site view to reduce API calls

--- app/poros/github_repo.rb
+++ app/poros/github_repo.rb
@@ -1,36 +1,23 @@
 class GithubRepo
-  attr_reader :commits_hash, :contributor_details
+  attr_reader :data
 
   def initialize
-    @repo_prs = GithubService.num_pull_requests
-    @contributors = GithubService.all_contributors
-    @thing = GithubService.connect_user
-    # @commits_hash = @commits.each_with_object(Hash.new(0)) do |commit, hash|
-    #   require 'pry'; binding.pry
-    #   if project_team.include? commit[:login]
-    #     hash[commit[:login]] += 1
-    #   end
-    # end
-    project_team = ['netia1128', 'suzkiee', 'jamogriff', 'Jaybraum']
-
-    @contributor_details = @contributors.each_with_object({}) do |login, hash|
-      if project_team.include? login[:login]
-        hash[login[:login]] = login[:contributions]
-      end
-    end
+    @data = GithubService.connect_repo
+    @pull_requests = GithubService.num_pull_requests
+    @team = GithubService.all_contributors
   end
 
   def num_merged_prs
-    @repo_prs.find_all do |pr|
+    @pull_requests.find_all do |pr|
       pr[:merged_at]
     end.count
   end
 
-  def contributor_details
-    project_team = ['netia1128', 'suzkiee', 'jamogriff', 'Jaybraum']
+  def contributors
+    team_usernames = ['netia1128', 'suzkiee', 'jamogriff', 'Jaybraum']
 
-    @contributor_details = @contributors.each_with_object({}) do |login, hash|
-      if project_team.include? login[:login]
+    @team.each_with_object({}) do |login, hash|
+      if team_usernames.include? login[:login]
         hash[login[:login]] = login[:contributions]
       end
     end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,3 @@
-require 'token'
-
 class GithubService
   REPO_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop'
   PRSPECIFIC_PATH = 'https://api.github.com/repos/suzkiee/little-esty-shop/pulls/74/commits'
@@ -8,7 +6,9 @@ class GithubService
 
   # This method can intake any path so API calls can be flexible.
   def self.connect(path)
-    validate_connection(Faraday.get(path))
+    begin
+      validate_connection(Faraday.get(path))
+    end
   end
 
   # The following two methods utilize pre-specified paths that are common
@@ -31,7 +31,9 @@ class GithubService
   # Could add better error if API response is not 200, but fine for our purposes
   def self.validate_connection(response)
     if !response.status == 200
-      # Incorporate flash alert here? yes
+      # Tried doing a StandardError or flash[:alert]
+      # but both were unrecognized commands
+      puts "Connection to GitHub Service Interrupted"
       nil
     else
       JSON.parse(response.body, symbolize_names: true)
